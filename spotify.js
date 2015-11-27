46a86bdcb756a5af906972ffbd5f41a1a43c1925
Ana Llorca <ana.llorca.a@gmail.com>
2015-11-27 10:03:36 +0100

    spotify exercise

--- spotify.js
+++ spotify.js
@@ -20,13 +20,38 @@ function searchArtist(query) {
 function findedArtists(artists) {
 	artists['artists']['items'].forEach(function displayArtist (artist) {
 		var artistName = artist.name;
-		var artistImages = artist.images[1]['url'];
+		var artistImages = artist.images[0].url;
 
 		var html = [
-			'<div style="text-align: left">'+artistName+'</div>',
-			'<div style="text-align: left"><img width="250" src="'+artistImages+'"></div>',
+			'<div>'+artistName+'</div>',
+			'<div><a class="image"><img width="250" src="'+artistImages+'"></a></div>',
 		].join('\n');
 
 		$('.container').append(html);
+	});
+};
+
+$(document).on('click', '.image',function() {
+	searchAlbum(artist)
+});
+
+
+function searchAlbum (artist) {
+	$.ajax({
+		url: 'https://api.spotify.com/v1/search?type=album&query=' + artist,
+		success: findedAlbums,
+		error: function() {
+			console.log('Error')
+		},
+		dataType: 'json'
+	});
+};
+
+function findedAlbums(albums) {
+	albums['albums']['items'].forEach(function displayAlbums (albums) {
+		var albumName = album.name;
+		var html = ['<div style="text-align: left">'+artistName+'</div>',].join('\n');
+		$('.album_container').append(html);
+
 	});
 };
